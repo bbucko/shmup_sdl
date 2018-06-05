03645a1a15e47354150284302eb2bb87732ff6b2
Blazej Bucko <blazej.bucko@gmail.com>
2018-06-05 21:24:48 +0200

    Maps generated by Tiled are now fully parsed

--- game/base/LevelParser.cpp
+++ game/base/LevelParser.cpp
@@ -46,52 +46,58 @@ Level *LevelParser::parseLevel(const char *levelFile) {
 
 void LevelParser::parseTilesets(XMLElement *pTilesetRoot, std::vector<Tileset> *pTilesets) {
     std::string source;
-    std::string firstGid;
+    Tileset tileset{};
 
     for (auto a = pTilesetRoot->FirstAttribute(); a; a = a->Next()) {
         if (StringUtils::equalsIgnoreCase(a->Name(), "source")) { source = std::string(m_dir) + "/" + a->Value(); }
-        if (StringUtils::equalsIgnoreCase(a->Name(), "firstgid")) { firstGid = a->Value(); }
+        if (StringUtils::equalsIgnoreCase(a->Name(), "firstgid")) { tileset.firstGridID = a->IntValue(); }
     }
 
     XMLDocument doc;
     auto result = doc.LoadFile(source.c_str());
-    LOG_INFO("open: " << source);
+
     if (result == XML_SUCCESS) {
         auto pRoot = doc.RootElement();
-        Tileset tileset;
 
-        std::string name;
         for (auto a = pRoot->FirstAttribute(); a; a = a->Next()) {
-            if (StringUtils::equalsIgnoreCase(a->Name(), "name")) { name = a->Value(); }
+            if (StringUtils::equalsIgnoreCase(a->Name(), "name")) { tileset.name = a->Value(); }
+            if (StringUtils::equalsIgnoreCase(a->Name(), "tilewidth")) { tileset.tileWidth = a->IntValue(); }
+            if (StringUtils::equalsIgnoreCase(a->Name(), "tileheight")) { tileset.tileHeight = a->IntValue(); }
+            if (StringUtils::equalsIgnoreCase(a->Name(), "spacing")) { tileset.spacing = a->IntValue(); }
+            if (StringUtils::equalsIgnoreCase(a->Name(), "margin")) { tileset.margin = a->IntValue(); }
+            if (StringUtils::equalsIgnoreCase(a->Name(), "columns")) { tileset.numColumns = a->IntValue(); }
         }
 
-        if (!name.empty()) {
-            for (auto *pElementRoot = pRoot->FirstChildElement(); pElementRoot != nullptr; pElementRoot = pElementRoot->NextSiblingElement()) {
-                auto elementValue = std::string(pElementRoot->Value());
-                if (StringUtils::equalsIgnoreCase(elementValue, "image")) {
-                    for (auto a = pTilesetRoot->FirstAttribute(); a; a = a->Next()) {
-                        if (StringUtils::equalsIgnoreCase(a->Name(), "source")) {
-                            auto filename = std::string(m_dir) + "/" + a->Value();
-                            ServiceLocator::textureManager()->load(filename, name, ServiceLocator::renderer());
+        for (auto *pElementRoot = pRoot->FirstChildElement(); pElementRoot != nullptr; pElementRoot = pElementRoot->NextSiblingElement()) {
+            auto elementValue = std::string(pElementRoot->Value());
+            if (StringUtils::equalsIgnoreCase(elementValue, "image")) {
+                for (auto a = pElementRoot->FirstAttribute(); a; a = a->Next()) {
+                    if (StringUtils::equalsIgnoreCase(a->Name(), "source")) {
+                        auto imageFilename = std::string(m_dir) + "/" + a->Value();
+                        LOG_INFO("Loading image: " << tileset.name << " from file: " << imageFilename);
 
-
-                        }
+                        ServiceLocator::textureManager()->load(imageFilename, tileset.name, ServiceLocator::renderer());
                     }
+
+                    if (StringUtils::equalsIgnoreCase(a->Name(), "width")) { tileset.width = a->IntValue(); }
+                    if (StringUtils::equalsIgnoreCase(a->Name(), "height")) { tileset.height = a->IntValue(); }
                 }
             }
+
+
+            LOG_INFO("Done creating tileset: " << tileset.name);
             pTilesets->push_back(tileset);
         }
     }
 }
 
-void LevelParser::parseTileLayer(XMLElement *pTileElement, std::vector<Layer *> *pLayers,
-                                 const std::vector<Tileset> *pTilesets) {
+void LevelParser::parseTileLayer(XMLElement *pTileElement, std::vector<Layer *> *pLayers, const std::vector<Tileset> *pTilesets) {
     std::string name;
-    int width, height;
+
     for (auto a = pTileElement->FirstAttribute(); a; a = a->Next()) {
         if (StringUtils::equalsIgnoreCase(a->Name(), "name")) { name = a->Value(); }
-        if (StringUtils::equalsIgnoreCase(a->Name(), "width")) { width = a->IntValue(); }
-        if (StringUtils::equalsIgnoreCase(a->Name(), "height")) { height = a->IntValue(); }
+        if (StringUtils::equalsIgnoreCase(a->Name(), "width")) { m_width = a->IntValue(); }
+        if (StringUtils::equalsIgnoreCase(a->Name(), "height")) { m_height = a->IntValue(); }
     }
 
     auto pTileLayer = new TileLayer(m_tileSize, *pTilesets);
--- tests/base_tests/LevelParserTest.cpp
+++ tests/base_tests/LevelParserTest.cpp
@@ -29,12 +29,27 @@ namespace {
     };
 
     TEST_F(LevelParserTest, ParserTest) {
-        EXPECT_CALL(manager, load("/tmp/shmup_tests/tiles.tsx", "tiles", _))
+        EXPECT_CALL(manager, load("/tmp/shmup_tests/tiles.png", "tiles", _))
                 .WillOnce(Return(true));
 
         auto pLevel = LevelParser::Instance().parseLevel("/tmp/shmup_tests/map1.tmx");
 
-        EXPECT_NE(pLevel, nullptr);
+        std::vector<Tileset> *pVector = pLevel->getTilesets();
+        EXPECT_EQ(pVector->size(), 1);
+
+        Tileset &tileset = pVector->at(0);
+        EXPECT_EQ(tileset.name, "tiles");
+        EXPECT_EQ(tileset.height, 67);
+        EXPECT_EQ(tileset.width, 196);
+        EXPECT_EQ(tileset.firstGridID, 1);
+        EXPECT_EQ(tileset.tileHeight, 64);
+        EXPECT_EQ(tileset.tileWidth, 64);
+        EXPECT_EQ(tileset.spacing, 1);
+        EXPECT_EQ(tileset.margin, 1);
+        EXPECT_EQ(tileset.numColumns, 3);
+
+        auto layers = pLevel->getLayers();
+        EXPECT_EQ(layers->size(), 1);
     }
 
 }
