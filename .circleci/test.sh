5451a36d91e1cbe57e87860ede91af00f8299c8d
Blazej Bucko <blazej.bucko@gmail.com>
2018-06-06 15:54:12 +0200

    Fixing gcovr

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +e
+set -e
 BUILD_DIR=build-$1
 cd $BUILD_DIR
 echo "Recompile if needed"
@@ -8,7 +8,7 @@ echo "Starting tests"
 mkdir -p $TEST_REPORTS
 echo "Saving tests results to $TEST_REPORTS"
 GTEST_OUTPUT="xml:$TEST_REPORTS/" CTEST_OUTPUT_ON_FAILURE=TRUE make test
+echo "Gathering coverage"
 make coverage
 cd ..
-gcovr . --exclude tests -x >> coverage.xml
-set -e
+gcovr . --exclude tests
